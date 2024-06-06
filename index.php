756923d1988e084447655054d41fe4a824caba39
Jhon Wesley <jwbf2016@gmail.com>
2024-06-06 20:23:11 -0300

    Primeira att do Projeto
    Att

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Dinâmico</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Blog Dinâmico</h1>
+    </header>
+    <main>
+        <section id="posts">
+
+        </section>
+    </main>
+    
+</body>
+</html>
