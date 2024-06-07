3f86bee15bfefdce89ce776a3346a06d154857a8
Jhon Wesley <jwbf2016@gmail.com>
2024-06-06 22:08:23 -0300

    att
    att css e começo de js

--- index.php
+++ index.php
@@ -12,9 +12,19 @@
     </header>
     <main>
         <section id="posts">
+            <?php
+            include 'db.php';
 
+            $result = $conn->query("SELECT * FROM posts");
+            While ($row = $result->fetch_assoc()) {
+                echo '<article>';
+                echo '<h2><a href=post.php?id=' . $row['id'] . '">' . $row['title'] . '</a></h2>';
+                echo '<p>' . substr($row['content'], 0, 100) . '...</p>';
+                echo '</article>';
+            }
+            ?>
         </section>
     </main>
-    
+    <script src="js/scripts.js"></script>
 </body>
 </html>
---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMcontentLoaded", function() {
+    console.log("Javascript carregado e pronto.");
+});
